0d0e0578dc0b71d75b51be559a46468ff3277146
samuelbrito2535 <samuelbrito2535@gmail.com>
2025-10-07 18:58:22 -0300

    Add files via upload

---
+++ projetovortex/projetovortex/src/test/java/br/com/criandoapivortex/projetovortex/ProjetovortexApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.criandoapivortex.projetovortex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetovortexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
