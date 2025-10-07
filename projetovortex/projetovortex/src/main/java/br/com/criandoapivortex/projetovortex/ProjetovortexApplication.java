5918d888ebb70d57a873581059ec07fce28ebe82
samuelbrito2535 <samuelbrito2535@gmail.com>
2025-10-07 18:59:56 -0300

    Add files via upload

---
+++ projetovortex/projetovortex/src/main/java/br/com/criandoapivortex/projetovortex/ProjetovortexApplication.java
@@ -0,0 +1,13 @@
+package br.com.criandoapivortex.projetovortex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetovortexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetovortexApplication.class, args);
+	}
+
+}
---
+++ projetovortex/projetovortex/src/main/java/br/com/criandoapivortex/projetovortex/cadastro/CadastroRequest.java
@@ -0,0 +1,14 @@
+package br.com.criandoapivortex.projetovortex.cadastro;
+
+public class CadastroRequest {
+
+        public String nome;
+        public String email;
+        public String senha;
+        public String telefone;
+
+
+
+        public String linkIndicacaoPai;
+    }
+
